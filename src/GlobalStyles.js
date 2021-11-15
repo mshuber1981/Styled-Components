3a1380e558a201485610f50aeae452352f10ed1c
Michael Huber <michael.huber1981@outlook.com>
2021-11-15 08:38:44 -0600

    Updating global styles

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,20 +1,32 @@
 import { createGlobalStyle } from "styled-components/macro";
 
 const GlobalStyles = createGlobalStyle`
-   :root {
+:root {
   --primary-color: #645cff;
   --white: #fff;
 }
 
-body {
+*,
+::after,
+::before {
   margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
     "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   background: #f2f4f8;
-} 
+}
+
+img {
+  width: 100%;
+  display: block;
+}
 `;
 
 export default GlobalStyles;
