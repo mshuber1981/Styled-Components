87253d57d8b8bb559f28cb0e50c798e31fd779a6
Michael Huber <michael.huber1981@outlook.com>
2021-11-25 09:12:59 -0600

    Updating styles, adding CSS helper example

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,20 +1,31 @@
-import styled from "styled-components/macro";
+import styled, { css } from "styled-components";
 
-export const DefualtButton = styled.button`
+export const DefaultButton = styled.button`
   background: var(--primary-color);
   color: #fff;
   border: none;
   border-radius: 0.25rem;
   cursor: pointer;
   text-transform: capitalize;
-  padding: 0.25rem;
   display: block;
-  width: 10rem;
-  margin: 1rem auto;
+  width: 12rem;
+  margin: 1rem 0;
+  /* https://styled-components.com/docs/api#css */
+  ${({ large }) =>
+    large
+      ? css`
+          padding: 1rem;
+          font-size: 1.5rem;
+          font-weight: 700;
+        `
+      : css`
+          padding: 0.5rem;
+          font-size: 1rem;
+        `}
 `;
 
 // Extending DefaultButton with additional styles
-export const HipsterButton = styled(DefualtButton)`
+export const HipsterButton = styled(DefaultButton)`
   width: 15rem;
   background: transparent;
   color: var(--primary-color);
